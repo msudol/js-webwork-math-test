1c7e418745e5ad4ebc7a366df4c01651bb65707b
msudol <msudol@anaren.com>
2018-01-26 10:08:11 -0500

    adding the code for this with some small tweaks from the original

---
+++ worker.js
@@ -0,0 +1,29 @@
+// incoming msg
+var msg = {};
+// outgoing msg
+var omsg = {};
+
+onmessage = function(e) {
+    // the passed-in data is available via e.data
+    var msg = JSON.parse(e.data);
+    if (msg.run) {
+        IPN(msg.n, msg.mod);
+    } 
+    else {
+        run = false;
+    }
+};
+
+// take a number ranger to operate on
+function IPN(n, mod) {
+    for (n; 0 < 1; n = n + mod) {
+        var s = n.toString(), x = 0;
+        for (var i = 0; i < s.length; i++) { 
+            x = x + Math.pow(s[i], s.length); 
+        }
+        if (x == n) {
+            // alert the main thread
+            postMessage(n);
+        }
+    }
+};
